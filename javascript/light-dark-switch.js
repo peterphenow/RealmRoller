c73c1bd942509a0cf40503c1ad699719dbd66d81
Benito Gonzalez <ohnobenito@gmail.com>
2020-11-10 21:00:22 -0600

    fixed issues

--- javascript/light-dark-switch.js
+++ javascript/light-dark-switch.js
@@ -2,12 +2,10 @@ function swapStyle(sheet) {
   document.getElementById("pagestyle").setAttribute("href", sheet);
 }
 
-function letsGo() {
-  let light = document.getElementById("stylesheet1");
-  light.onclick = swapStyle("css/light-mode.css");
-}
+$("#light").on("click", function(){
+  swapStyle("css/light-mode.css");
+});
 
-function letsGo2() {
-  let dark = document.getElementById("stylesheet2");
-  dark.onclick = swapStyle("css/dark-mode.css");
-}
+$("#dark").on("click", function(){
+  swapStyle("css/dark-mode.css");
+})
